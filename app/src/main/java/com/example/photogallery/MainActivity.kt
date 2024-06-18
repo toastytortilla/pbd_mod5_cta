eda7c6117cc5ed316215058bf063c254666045f7
Daniel DiCocco <dvdicocco@gmail.com>
2024-06-18 16:45:57 -0400

    Testing change tracking again

--- app/src/main/java/com/example/photogallery/MainActivity.kt
+++ app/src/main/java/com/example/photogallery/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : AppCompatActivity() {
             ImageItem("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F57%2F2d%2F72%2F572d729591618688d70cb64705c906b6.jpg&f=1&nofb=1&ipt=2fbeabe6c199230e20b9c66c989a02b4e1cc72da9f8c177f0547db733ed912c4&ipo=images")
         )
 
+        // Connect ImageAdapter.kt
         imageAdapter = ImageAdapter(this, images)
         recyclerView.adapter = imageAdapter
     }
